b833d63e7d1d2ca7d2ea0a06437c3ad2c943b25f
Florian MORGAN <florian.morgan@gmail.com>
2015-07-04 15:53:13 +0200

    Added line of sight

--- profile.py
+++ profile.py
@@ -30,23 +30,34 @@ LOGGER = logging.getLogger(os.path.basename(__file__))
 # TODO enforce input arguments
 # TODO add radius correction based on the latitude, see: http://en.wikipedia.org/wiki/Earth_radius#Geocentric_radius
 # TODO currently only 'sampling', to be 'exact' a full path should be performed on the actual dataset
-# TODO rasterize a polyline, see: http://gis.stackexchange.com/questions/97306/rasterizing-polyline-data-with-qgis-gdal-custom-line-width
+# TODO rasterize a polyline:
+# see: http://gis.stackexchange.com/questions/97306/rasterizing-polyline-data-with-qgis-gdal-custom-line-width
 
-def profile(data_source, wgs84_lat1, wgs86_long1, wgs84_lat2, wgs84_long2, definition=512):
+def profile(data_source, wgs84_lat1, wgs86_long1, wgs84_lat2, wgs84_long2,
+            height1=0, height2=0, above_ground1=True, above_ground2=True, definition=512):
     elevations = []
     half_central_angle = geometry.half_central_angle(math.radians(wgs84_lat1), math.radians(wgs86_long1),
                                                      math.radians(wgs84_lat2), math.radians(wgs84_long2))
     max_overhead = geometry.overhead_height(half_central_angle, geometry.EARTH_RADIUS)
+    start_sight = height1
+    if above_ground1:
+        start_sight += geods.read_ds_value_from_wgs84(data_source, wgs84_lat1, wgs86_long1)
+
+    end_sight = height2
+    if above_ground2:
+        end_sight += geods.read_ds_value_from_wgs84(data_source, wgs84_lat2, wgs84_long2)
 
     for i in range(definition+1):
         i_lat = wgs84_lat1 + ((wgs84_lat2 - wgs84_lat1) * i) / definition
         i_long = wgs86_long1 + ((wgs84_long2 - wgs86_long1) * i) / definition
+        i_sight = start_sight + ((end_sight - start_sight) * i) / definition
         elevation = geods.read_ds_value_from_wgs84(data_source, i_lat, i_long)
         distance = geometry.distance_between_wgs84_coordinates(wgs84_lat1, wgs86_long1, i_lat, i_long)
         angle = 2 * geometry.half_central_angle(math.radians(wgs84_lat1), math.radians(wgs86_long1),
                                                 math.radians(i_lat), math.radians(i_long))
         overhead = max_overhead - geometry.overhead_height(half_central_angle - angle, geometry.EARTH_RADIUS)
-        elevations.append({'lat': i_lat, 'long': i_long, 'distance': distance, 'elevation': elevation, 'overhead': overhead})
+        elevations.append({'lat': i_lat, 'long': i_long, 'distance': distance, 'elevation': elevation,
+                           'overhead': overhead, 'sight': i_sight})
 
     return elevations
 
